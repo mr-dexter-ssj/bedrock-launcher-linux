a73ce1b2d1a32629e08115259f59ea2d7fbe1757
mrdexterssj <137222384+mr-dexter-ssj@users.noreply.github.com>
2026-05-25 21:56:21 -0500

    Add logic for downloading ProxyPass and ProtonGDK

--- src/functions/install.py
+++ src/functions/install.py
@@ -6,10 +6,11 @@ import subprocess
 import os
 import traceback
 import sys
+import tarfile
 
 class WorkerSignals(QObject):
     finished = Signal(tuple)
-    error = Signal(tuple)
+    error = Signal(object)
     result = Signal(object)
     progress = Signal(tuple)
 
@@ -23,16 +24,30 @@ class Worker(QRunnable):
         self.kwargs = kwargs
     @Slot()
     def run(self):
-        #self.fn(self.configPath)
-        self.fn(*self.args, **self.kwargs)
-        self.signals.finished.emit(self.threadId)
+        try:
+            self.fn(*self.args, **self.kwargs)
+        except Exception as e:
+            self.signals.error.emit(e)
+        ###self.signals.finished.emit(self.threadId)
        
 
 @Slot()
 def install(window, configPath, app):
     threadId = 0
-    exctype, value = 0
+    ###exctype, value = "0", "0"
     threadCount = window.threadpool.maxThreadCount()
+
+
+    #####Error handling#####
+    def errorHandler(e):
+        #Display error message (Not implemented)
+        ###sys.exit()
+        print("An error has ocurred: " + str(e))
+        QApplication.quit()
+
+    
+    #Configure starting date-time
+    window.outputView.append(f"[{str(datetime.datetime.now())[:-7]}] {window.outputView.placeholderText()}")
     window.outputView.append(f"[DEBUG] Multithreading with maximum {threadCount} threads")
 
     #Check for umu-launcher
@@ -43,12 +58,9 @@ def install(window, configPath, app):
         window.outputView.append(f"Found umu-launcher version {umuVersion}")
     except Exception as e:
         window.outuptView.append("Failed umu-launcher check. The installer can't continue." + str(e))
-        #sys.exit() --------->   (I feel it's more brute force, so I do app.quit())
-        app.quit()
+        ###sys.exit()
+        errorHandler(e)
     
-    #Configure starting date-time
-    window.outputView.append(f"[{str(datetime.datetime.now())[:-7]}] {window.outputView.placeholderText()}")
-
     #Determine necessary files
     window.outputView.append("Determining necessary files...")
     xcurl = window.xcurlConsentCheck.isChecked()
@@ -65,17 +77,46 @@ def install(window, configPath, app):
         window.outputView.append("Success!")
     except Exception as jvmCheckFailed:
        window.outputView.append("[WARNING] Java check failed:" + str(jvmCheckFailed))
+
+    #####Make "/tools/"#####
+    toolsFolder = configPath + "/tools"
+    if not os.path.exists(toolsFolder):
+        os.mkdir(toolsFolder)
+        window.outputView.append(f"Created directory {toolsFolder}")
     
     #####Download ProtonGDK#####
-    #Note: This downloads the latest fix of ProtonGDK (lucaspah fork). If needed, this can be changed to a .json database.
-    def downloadProtonGDK():
+    #downloadProtonGDK() -> the main downloading logic
+    #printResultProtonGDK() -> prints the result to the outputView text edit
+    #downloadProtonGDKThreaded() -> spawns a new thread and runs downloadProtonGDK() there. (See the Worker() class on top of the file for more info)
+    #Note: This downloads the latest fix of ProtonGDK (lucaspah fork) and extracts it. If needed, this can be changed to a .json database.
+    def downloadProtonGDK(configPath):
         protonGDKLink = "https://github.com/LukasPAH/GDK-Proton-Custom/releases/download/release-10-32-3/GDK-Proton10-32-Custom-3.tar.gz"
         try:
             protonGDKTarGz = requests.get(protonGDKLink)
         except Exception as e:
-            print("Hi")
-        
-
+            raise(e)
+        protonGDKFolder = toolsFolder + "/ProtonGDK/"
+        if not os.path.exists(protonGDKFolder):
+            os.mkdir(protonGDKFolder)
+        try:
+            open(os.path.expanduser(protonGDKFolder) + "ProtonGDK.tar.gz", 'wb').write(protonGDKTarGz.content)
+            window.protonWorker.signals.finished.emit(threadId)
+        except Exception as e:
+            raise(e)
+        try:
+            tarfileProtonGDK = tarfile.open(protonGDKFolder + "ProtonGDK.tar.gz")
+            tarfileProtonGDK.extractall(protonGDKFolder + "ProtonGDK")
+            tarfileProtonGDK.close()
+        except Exception as e:
+            raise(e)
+    def printResultProtonGDK():
+        window.outputView.append("Finished downloading ProtonGDK (lucaspah fork)")
+    def downloadProtonGDKThreaded(fn):
+        window.protonWorker = Worker(fn, configPath)
+        window.threadpool.start(window.protonWorker)
+        window.protonWorker.signals.finished.connect(printResultProtonGDK)
+        window.protonWorker.signals.error.connect(errorHandler)
+    ###########################################################################################################
 
     #####Download ProxyPass#####
     #downloadProxyPass() -> the main downloading logic
@@ -85,33 +126,27 @@ def install(window, configPath, app):
         proxyPassUrl = "https://github.com/Kas-tle/ProxyPass/releases/download/master-65/ProxyPass.jar"
         try:
             proxyPassJar = requests.get(proxyPassUrl)
-        except Exception:
-            traceback.print_exc()
-            exctype, value = sys.exc_info()[:2]
-            window.worker.signals.error.emit((exctype, value, traceback.format_exc()))
-        proxyPassFolder = configPath + "/ProxyPass/"
+        except Exception as e:
+            raise(e)
+        proxyPassFolder = toolsFolder + "/ProxyPass/"
         if not os.path.exists(proxyPassFolder):
-            os.mkdir(proxyPassFolder)
+            os.makedirs(proxyPassFolder)
         try:
             open(os.path.expanduser(proxyPassFolder) + "ProxyPass.jar", 'wb').write(proxyPassJar.content)
+            window.worker.signals.finished.emit(threadId)
         except Exception:
-            traceback.print_exc()
-            exctype, value = sys.exc_info()[:2]
-            window.worker.signals.error.emit((exctype, value, traceback.format_exc()))
+            raise(e)
     @Slot()
     def printResultProxyPass(threadId):
         window.outputView.append(f"Finished downloading ProxyPass. On thread {threadId}.")
-    @Slot()
-    def printErrorProxyPass(*args):
-        window.outputView.append(f"Error downloading ProxyPass. {exctype}. {value}")
-        app.quit() #This will be changed to window dialog displaying error info and close button to close.
     def downloadProxyPassThreaded(fn):
         window.worker = Worker(fn, configPath)
         window.threadpool.start(window.worker)
-        window.worker.signals.error.connect(printErrorProxyPass)
-        window.worker.signals.finished.connect(printResultProxyPass) 
+        window.worker.signals.finished.connect(printResultProxyPass)
+    ###########################################################################################################
+    
+    downloadProtonGDKThreaded(downloadProtonGDK)
     if proxyPass:
         downloadProxyPassThreaded(downloadProxyPass)
     #if xcurl:
     #    downloadXCurlThreaded()
-
